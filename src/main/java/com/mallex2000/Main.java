5e017c8f99137f65218e8748cc982c080683d2b8
Sven Malchau <sven.malchau@business4enterprise.de>
2022-08-15 01:03:02 +0200

    init

--- src/main/java/com/mallex2000/Main.java
+++ src/main/java/com/mallex2000/Main.java
@@ -15,7 +15,6 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 //https://www.browserstack.com/docs/automate/selenium/getting-started/java#introduction
 //https://www.browserstack.com/guide/selenium-webdriver-tutorial
@@ -74,7 +73,7 @@ public class Main {
 		if (browser.equalsIgnoreCase("firefox")) {
 			// https://www.guru99.com/gecko-marionette-driver-selenium.html
 			FirefoxOptions options = new FirefoxOptions();
-			options.setCapability("marionette", true);
+//			options.setCapability("marionette", true);
 			driver = new FirefoxDriver(options);
 		}
 //		driver.manage().window().setSize(new Dimension(1024,768));
