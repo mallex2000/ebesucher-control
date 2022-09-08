4a4d27026272df85749f02c8541ddc0db1194b1e
Sven Malchau <sven.malchau@business4enterprise.de>
2022-09-08 15:37:19 +0200

    ebesucher update

---
+++ starter_minipc1.bat
@@ -0,0 +1,3 @@
+
+echo start
+C:\tools\jdk-19\bin\java -jar C:\Users\user\git\ebesucher-control\target\ebesucher-control-0.0.1-jar-with-dependencies.jar "C:\\Users\\user\\git\\ebesucher-control\\driver"   "mallex2000.minipc1" "chrome" 10
